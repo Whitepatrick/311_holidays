7fce4254af76003bf7b125300ece6d107bc0cf8c
pwhite <Patrick White>
2016-01-14 12:16:45 -0500

    adding some stack scripts for work and home, maybe won't work on mac

---
+++ homestack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "WORK VERSION"
+echo "stopping ipython if running"
+sudo docker stop ipython
+echo "removing ipython container if lingering"
+sudo docker rm ipython
+echo "building docker image for ipython from file"
+sudo docker build -t ipython docker/.
+echo "now running ipython"
+sudo docker run -d -p 8888:8888 -v $PWD:/workspace -w="/workspace" -n ipython jupyter/datascience-notebook start-notebook.sh
